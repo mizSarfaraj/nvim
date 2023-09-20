28b54543c11f1204e05ec778043bb3fca2b5203e
RamizSarfaraj <69590366+RamizSarfaraj@users.noreply.github.com>
2023-09-20 23:09:10 +0530

    formatting in neovim using lsp

--- lua/ramiz/plugins/harpoon.lua
+++ lua/ramiz/plugins/harpoon.lua
@@ -1,15 +1,19 @@
 return {
     "theprimeagen/harpoon",
-    config = function ()
+    config = function()
         local mark = require("harpoon.mark")
         local ui = require("harpoon.ui")
 
-        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd [F]ile To Harpoon" })
-        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "[O]pen Harpoon Quick Menu" })
+        vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add File To Harpoon" })
+        vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Open Harpoon Quick Menu" })
 
-        vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end, { desc = '[N]avigate To File Number 1' })
-        vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end, { desc = '[N]avigate To File Number 2' })
-        vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end, { desc = '[N]avigate To File Number 3' })
-        vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end, { desc = '[N]avigate To File Number 4' })
+        vim.keymap.set("n", "<M-l>", function() ui.nav_next() end, { desc = "Go To Harpoon Next Mark" })
+        vim.keymap.set("n", "<M-h>", function() ui.nav_prev() end, { desc = "Go To Harpoon Previous Mark" })
+
+        vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end, { desc = 'Navigate To Harpoon File Number 1' })
+        vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end, { desc = 'Navigate To Harpoon File Number 2' })
+        vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end, { desc = 'Navigate To Harpoon File Number 3' })
+        vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end, { desc = 'Navigate To Harpoon File Number 4' })
+        vim.keymap.set("n", "<M-5>", function() ui.nav_file(5) end, { desc = 'Navigate To Harpoon File Number 4' })
     end
 }
--- lua/ramiz/plugins/lsp/lspconfig.lua
+++ lua/ramiz/plugins/lsp/lspconfig.lua
@@ -14,13 +14,14 @@ return {
 
         local keymap = vim.keymap -- for conciseness
 
-        local opts = { noremap = true, silent = true }
+        -- local opts = { noremap = true, silent = true }
         local on_attach = function(client, bufnr)
             local opts = { buffer = bufnr, remap = false }
 
             keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts, { desc = "Go To Definition" })
             keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts, { desc = "Hover Menu (LSP)" })
-            keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts, { desc = "Find Worksplace Symbol" })
+            keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts,
+                { desc = "Find Worksplace Symbol" })
             keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts, { desc = "" })
             keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts, { desc = "" })
             keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts, { desc = "" })
@@ -29,8 +30,12 @@ return {
             keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts, { desc = "" })
             keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts, { desc = "" })
             -- keymap.set('n', '<S-M-f>', vim.cmd.LspZeroFormat, opts, { desc = "[F]ormat File" })
-            keymap.set('n', '<leader>lr', function() require('telescope.builtin').lsp_references() end, { noremap = true, silent = true, desc = "Find References in Telescope" })
-
+            keymap.set('n', '<leader>lr', function() require('telescope.builtin').lsp_references() end,
+                { noremap = true, silent = true, desc = "Find References in Telescope" })
+            keymap.set('n', '<M-S-f>', function()
+                vim.lsp.buf.format()
+                print("Formatted")
+            end, { desc = "Format Buffer" })
         end
 
         -- used to enable autocompletion (assign to every lsp server config)
@@ -40,54 +45,54 @@ return {
         -- (not in youtube nvim video)
         -- local signs = { Error = "¿ ", Warn = "¿ ", Hint = "¿ ", Info = "¿ " }
         -- for type, icon in pairs(signs) do
-          -- local hl = "DiagnosticSign" .. type
-          -- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+        -- local hl = "DiagnosticSign" .. type
+        -- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         -- end
 
         -- configure html server
         lspconfig["html"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
 
         -- configure typescript server with plugin
         lspconfig["tsserver"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
 
         -- configure css server
         lspconfig["cssls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
+            capabilities = capabilities,
+            on_attach = on_attach,
         })
 
         -- configure emmet language server
         lspconfig["emmet_ls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
         })
 
         -- configure lua server (with special settings)
         lspconfig["lua_ls"].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = { -- custom settings for lua
-            Lua = {
-              -- make the language server recognize "vim" global
-              diagnostics = {
-                globals = { "vim" },
-              },
-              workspace = {
-                -- make language server aware of runtime files
-                library = {
-                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                  [vim.fn.stdpath("config") .. "/lua"] = true,
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = { -- custom settings for lua
+                Lua = {
+                    -- make the language server recognize "vim" global
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        -- make language server aware of runtime files
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        },
+                    },
                 },
-              },
             },
-          },
         })
-        end,
+    end,
 }
